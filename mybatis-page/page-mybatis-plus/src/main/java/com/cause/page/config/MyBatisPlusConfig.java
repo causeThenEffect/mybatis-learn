5baf170fdcbbfafe906575e8d34acb8693ec4e07
causeThenEffect <songzhipeng@loopin.group>
2023-08-02 15:15:41 +0800

    springboot add plugin

--- mybatis-page/page-mybatis-plus/src/main/java/com/cause/page/config/MyBatisPlusConfig.java
+++ mybatis-page/page-mybatis-plus/src/main/java/com/cause/page/config/MyBatisPlusConfig.java
@@ -1,6 +1,7 @@
 package com.cause.page.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,14 @@ public class MyBatisPlusConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
+    }
+
+    @Bean
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+//        performanceInterceptor.setMaxTime(1);
+//        performanceInterceptor.setWriteInLog(true);
+        return performanceInterceptor;
     }
 
 }
